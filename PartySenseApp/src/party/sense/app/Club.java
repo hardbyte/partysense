61a296676749135eb13e110c70ce981589a5b2e4
Tanmay Bhola <tanmay9@gmail.com>
2012-12-08 18:17:27 +1300

    Updated the Club and User Data Models

--- PartySenseApp/src/party/sense/app/Club.java
+++ PartySenseApp/src/party/sense/app/Club.java
@@ -11,20 +11,34 @@ import java.util.List;
  *
  */
 public class Club {
-	private String mName;
-	private String mAddress;
-	private String mPhoneNumber;
-	private String mEmail;
-	private String mWebsite;
-	private String mFwitterName;
-	private String mFacebook;
-	private String mGPlus;
-	private String mOpenDuring;
-	private long mLatitude;
-	private long mLongitude;
-	private List<String> mPhotoUrls;
-	private List<PartySenseTag> mTags;
-	private String mDescription;
-	private String mUnstructuredInformation;
+	private String name;
+	private String address;
+	private String phone_number;
+	private String email;
+	private String website;
+	private String twitter;
+	private String facebook;
+	private String google_plus;
 	
+	private String time_created;
+	private String time_updated;
+	
+	private long latitude;
+	private long longitude;
+	
+	private List<String> photo_urls;
+	
+	private List<String> tags;
+	
+	private String description;
+	private String unstructured_data;
+	
+	private String hours_default;
+	private String hours_monday;
+	private String hours_tuesday; 
+	private String hours_wednesday; 
+	private String hours_thursday; 
+	private String hours_friday; 
+	private String hours_saturday;
+	private String hours_sunday;
 }
