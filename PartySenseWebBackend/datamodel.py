39e4a63350286c7e3bf4c9a645e8a9318a32a4e9
Robert King <kingrobertking@gmail.com>
2012-12-08 18:31:48 +1300

    added admin forms for persisting data and json api for restful json sync

---
+++ PartySenseWebBackend/datamodel.py
@@ -0,0 +1,68 @@
+__author__ = 'robert'
+
+from google.appengine.ext import ndb
+
+class Club(ndb.Model):
+    name = ndb.StringProperty(required=True)
+    address = ndb.StringProperty()
+    phone_number = ndb.StringProperty()
+    email = ndb.StringProperty()
+    website = ndb.StringProperty()
+    twitter = ndb.StringProperty()
+    facebook = ndb.StringProperty()
+    google_plus = ndb.StringProperty()
+
+    time_created = ndb.DateTimeProperty(auto_now_add=True)
+    time_updated = ndb.DateTimeProperty(auto_now=True)
+
+    latitude = ndb.FloatProperty()
+    longitude = ndb.FloatProperty()
+
+    photo_blob_keys = ndb.BlobKeyProperty(repeated=True)
+    photo_urls = ndb.StringProperty(repeated=True)
+
+    tags = ndb.StringProperty(repeated=True)
+
+    description = ndb.StringProperty()
+
+    unstructured_data = ndb.TextProperty()
+
+    hours_default = ndb.StringProperty()
+    hours_monday = ndb.StringProperty()
+    hours_tuesday = ndb.StringProperty()
+    hours_wednesday = ndb.StringProperty()
+    hours_thursday = ndb.StringProperty()
+    hours_friday = ndb.StringProperty()
+    hours_saturday = ndb.StringProperty()
+    hours_sunday = ndb.StringProperty()
+
+class User(ndb.Model):
+    name = ndb.StringProperty()
+    age = ndb.StringProperty()
+    email = ndb.StringProperty()
+    clubs_favorite = ndb.StringProperty(repeated=True)
+    clubs_visited = ndb.StringProperty(repeated=True)
+    favorite_genres = ndb.StringProperty(repeated=True)
+    friends_registered = ndb.KeyProperty(kind='User')
+    friends_facebook = ndb.StringProperty(repeated=True)
+
+    time_last_updated = ndb.DateTimeProperty() #time since last sync
+
+    latitude = ndb.FloatProperty()
+    longitude = ndb.FloatProperty()
+    time_location_updated = ndb.DateTimeProperty()
+
+    facebook_id = ndb.StringProperty()
+    spotify_id = ndb.StringProperty()
+    last_fm_id = ndb.StringProperty()
+
+    profile_picture_blob_key = ndb.BlobKeyProperty()
+    profile_picture_url = ndb.StringProperty()
+
+class Tag(ndb.Model):
+    tags = ndb.StringProperty(repeated=True)
+    #underscore separated
+    #e.g. [genre_hiphop, genre_dance, genre_pop, age_20-25, age_25-30]
+
+class TrendingClubs(ndb.Model):
+    clubs = ndb.KeyProperty(kind=Club, repeated=True)
--- PartySenseWebBackend/main.py
+++ PartySenseWebBackend/main.py
@@ -7,6 +7,9 @@ from google.appengine.ext import blobstore
 from google.appengine.ext.webapp import blobstore_handlers
 import cgi
 import quopri
+from datamodel import Club
+import json
+
 class BaseHandler(webapp2.RequestHandler):
     @webapp2.cached_property
     def jinja2(self):
@@ -25,7 +28,6 @@ class HomeHandler(BaseHandler):
         template = {}
         self.render_template('home.html', **template)
 
-
 class AdminHandler(BaseHandler):
     def get(self):
         template = {}
@@ -35,10 +37,105 @@ class SampleJson(BaseHandler):
     def get(self):
         self.response.write('{"message": "hello world"}')
 
+class BrowseDataModel(BaseHandler):
+    def get(self):
+        kinds = ["Club", "User", "Tag", "TrendingClubs"]
+        models = []
+        for kind in kinds:
+            models.append("<hr/><h3>" + kind + "</h3>" + "<br/>".join(get_lines(kind)))
+
+        self.response.write('<br/>'.join(models))
+
+class EditKindHandler(BaseHandler):
+    def get(self, kind):
+        template = {'form': get_form(kind)}
+        self.render_template('edit-kind.html', **template)
+
+class JsonHandler(BaseHandler):
+    def get(self, kind):
+        self.response.write(get_json(kind))
+
+def get_lines(kind):
+    end1 = "class "
+    end2 = "(ndb.Model):"
+    with open('datamodel.py') as f:
+        lines = f.readlines()
+    append = False
+    chunk = []
+    for line in lines:
+        if append:
+            if end1 in line and end2 in line:
+                break
+            chunk.append(line)
+        if "class " + kind + "(ndb.Model):" in line:
+            append = True
+
+    return chunk
+
+def get_form(kind):
+
+    lines = get_lines(kind)
+    form_lines = []
+    form_lines.append("<form>")
+    for line in lines:
+        line = line.strip()
+        if not line or line.startswith("#") or "=" not in line:
+            continue
+        is_string = "StringProperty" in line
+        is_repeat = "repeated=True" in line
+        is_int = "IntegerProperty" in line
+        is_float = "FloatProperty" in line
+        is_text = "TextProperty" in line
+        is_key = "Key" in line
+        is_date = "DateTimeProperty" in line
+        assert [is_date, is_string, is_int, is_float, is_key, is_text].count(True) == 1
+        if is_key or is_date:
+            continue
+        label = line.split("=")[0].strip()
+        input_ = """<input type="text" name="{0}">""".format(label)
+        if is_repeat:
+            input_ = input_ * 5
+        input_ += "<br/>"
+        form_line = "{label}: {input_}".format(label=label, input_=input_)
+        form_lines.append(form_line)
+    form_lines.append("""<input type="submit" value="Submit">""")
+    form_lines.append("</form>")
+    return "".join(form_lines)
+
+def get_json(kind):
+    lines = get_lines(kind)
+    json_dicts = []
+    #print lines, 'wtf'
+    for xx in "abc":
+        json_dict = {}
+        for line in lines:
+            line = line.strip()
+            if not line or line.startswith("#") or "=" not in line:
+                continue
+            is_string = "StringProperty" in line
+            is_repeat = "repeated=True" in line
+            is_int = "IntegerProperty" in line
+            is_float = "FloatProperty" in line
+            is_text = "TextProperty" in line
+            is_key = "Key" in line
+            is_date = "DateTimeProperty" in line
+            assert [is_date, is_string, is_int, is_float, is_key, is_text].count(True) == 1
+            if is_key or is_date:
+                continue
+            label = line.split("=")[0].strip()
+            #print label
+            json_dict[label] = xx
+        json_dicts.append(json_dict)
+    return json.dumps(json_dicts)
+
+
 _routes = [
     RedirectRoute('/', HomeHandler, name="home", strict_slash=True),
     RedirectRoute('/error/<message>/', ErrorHandler, name="error", strict_slash=True),
     RedirectRoute('/admin', AdminHandler, name="admin", strict_slash=True),
-    RedirectRoute('/api/sample-json', SampleJson, name="sample-json", strict_slash=True)
+    RedirectRoute('/api/sample-json', SampleJson, name="sample-json", strict_slash=True),
+    RedirectRoute('/api/all/<kind>', JsonHandler, name="json", strict_slash=True),
+    RedirectRoute('/admin/browse-data-model', BrowseDataModel, name="browse-data-model", strict_slash=True),
+    RedirectRoute('/admin/edit-kind/<kind>', EditKindHandler, name="edit-kind", strict_slash=True)
 ]
 app = webapp2.WSGIApplication(_routes, debug=True)
