a902d87201779edcaa0f404f73b03b5b7439dfe1
Tanmay Bhola <tanmay9@gmail.com>
2012-12-09 14:33:45 +1300

    Started implementing write / read from Internal storage

--- PartySenseApp/src/party/sense/app/SplashActivity.java
+++ PartySenseApp/src/party/sense/app/SplashActivity.java
@@ -1,12 +1,18 @@
 package party.sense.app;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
-import android.widget.Toast;
+import android.util.Log;
 
 public class SplashActivity extends Activity {
 
@@ -40,6 +46,40 @@ public class SplashActivity extends Activity {
 	    	}
 	    };
 	    splashTimerThread.start();
+	    
+	    // Adding functionality to Read/ Write to internal storage
+	    String myString = "Testing writing to Club DB";
+	    File partySenseDir = this.getDir("PartySenseData", Context.MODE_PRIVATE); //Creating an internal dir;
+	    File clubDbFile = new File(partySenseDir, "clubDbTest.json"); //Getting a file within the dir.
+	    FileOutputStream out = null;
+	    try {
+			out = new FileOutputStream(clubDbFile);
+			out.write(myString.getBytes());
+			out.close();
+		} catch (FileNotFoundException fnfe) {
+			fnfe.printStackTrace();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+		
+		FileInputStream in = null;
+		try {
+			in = openFileInput("filename.txt");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	    InputStreamReader inputStreamReader = new InputStreamReader(in);
+	    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+	    StringBuilder sb = new StringBuilder();
+	    String line;
+	    try {
+			while ((line = bufferedReader.readLine()) != null) {
+			    sb.append(line);
+			}
+			Log.e("SplashActivity", "Read : " + line);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
