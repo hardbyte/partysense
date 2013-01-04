e5d6656cc8ce0db531f72cf5e7a29aad1a23be8f
Devatanu Deka <deva.deka@gmail.com>
2013-01-05 12:05:00 +1300

    fixed null pointer bug for addClubItem function

--- PartySenseApp/src/party/sense/app/FragmentSongsScreen.java
+++ PartySenseApp/src/party/sense/app/FragmentSongsScreen.java
@@ -29,14 +29,18 @@ public class FragmentSongsScreen extends Fragment {
 		lvClubList = (ListView) view.findViewById(R.id.recommended_club_listview);
 		
 		ArrayList<ClubListItem> clubListArray = new ArrayList<ClubListItem>();
-		clubListArray.add(new ClubListItem(R.drawable.item_bg, clubList.get(0).getName(),"ELECTRO/HOUSE"));
+		/*clubListArray.add(new ClubListItem(R.drawable.item_bg, clubList.get(0).getName(),"ELECTRO/HOUSE"));
 		clubListArray.add(new ClubListItem(R.drawable.item_bg, "1885 Britomart","JAZZ/BLUES/FUNK"));
-		clubListArray.add(new ClubListItem(R.drawable.item_bg, "A'isha","UNDERGROUND"));
-		clubListArray.add(new ClubListItem(R.drawable.item_bg, "Club DELUXE","HOUSE/POP/RNB"));
+		clubListArray.add(new ClubListItem(R.drawable.item_bg, "A'isha","UNDERGROUND"));*/
+		clubListArray.add(new ClubListItem(R.drawable.item_bg, "Club TANMAY","BHANGRA/POP/RNB"));
 		
-		ClubListItemAdapter adapter = new ClubListItemAdapter(getActivity(), R.layout.club_item, clubListArray);
+		adapter = new ClubListItemAdapter(getActivity(), R.layout.club_item, clubListArray);
 		
 		lvClubList.setAdapter(adapter);
+		
+		for(Club c: clubList){
+			addClubItem(c);
+		}
 		
 		//edtView.setInputType(0);
 		
@@ -50,12 +54,15 @@ public class FragmentSongsScreen extends Fragment {
 	}
 	
 	private void addClubItem(Club c){
-		String genreString = "d";
-		/*for(String tags : c.getTags()){
-			if (tags.split("_")[0]=="genre"){
-				genreString += "/" + tags.split("_")[1]; 
+		String genreString ="";
+		for(String tags : c.getTags()){
+			if (tags.split("_")[0].toLowerCase().equals("music")){
+				genreString += "/" + tags.split("_")[1].toUpperCase(); 
 			}
-		}*/
-		adapter.add(new ClubListItem(R.drawable.item_bg, "Degree","ELECTRO/HOUSE"));	
+		}
+		if (genreString.length()>1){
+			genreString = genreString.substring(1);
+		}
+		adapter.add(new ClubListItem(R.drawable.item_bg, c.getName(),genreString));	
 	}
 }
