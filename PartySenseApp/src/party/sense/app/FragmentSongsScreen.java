c6843b191ed1dfa5b57a3f05f360dee3f7f02008
Devatanu Deka <deva.deka@gmail.com>
2013-01-05 14:46:28 +1300

    Club details page now shows description, genre and address

--- PartySenseApp/src/party/sense/app/FragmentSongsScreen.java
+++ PartySenseApp/src/party/sense/app/FragmentSongsScreen.java
@@ -2,12 +2,14 @@ package party.sense.app;
 
 import java.util.ArrayList;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -43,7 +45,17 @@ public class FragmentSongsScreen extends Fragment {
 		
 		//edtView.setInputType(0);
 		
-		
+		lvClubList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id) {
+            	Intent detailIntent = new Intent("android.intent.action.PartySenseDetailsActivity");
+            	detailIntent.putExtra(PartySenseDetailsActivity.BUNDLE_ID_CLUB_NAME, clubList.get(pos).getName());
+            	detailIntent.putExtra(PartySenseDetailsActivity.BUNDLE_ID_CLUB_GENRE, getGenreString(clubList.get(pos).getTags()));
+            	detailIntent.putExtra(PartySenseDetailsActivity.BUNDLE_ID_CLUB_ADDRESS, clubList.get(pos).getAddress());
+            	detailIntent.putExtra(PartySenseDetailsActivity.BUNDLE_ID_CLUB_WEBSITE, clubList.get(pos).getWebsite());
+            	detailIntent.putExtra(PartySenseDetailsActivity.BUNDLE_ID_CLUB_DESCRIPTION, clubList.get(pos).getDescription());
+            	startActivity(detailIntent);
+            }
+		});
 		
 		return view;
 	}
@@ -53,8 +65,13 @@ public class FragmentSongsScreen extends Fragment {
 	}
 	
 	private void addClubItem(Club c){
+		
+		adapter.add(new ClubListItem(R.drawable.item_bg, c.getName(),getGenreString(c.getTags())));	
+	}
+	
+	private String getGenreString(ArrayList<String> tagsList) {
 		String genreString ="";
-		for(String tags : c.getTags()){
+		for(String tags : tagsList){
 			if (tags.split("_")[0].toLowerCase().equals("music")){
 				genreString += "/" + tags.split("_")[1].toUpperCase(); 
 			}
@@ -62,6 +79,6 @@ public class FragmentSongsScreen extends Fragment {
 		if (genreString.length()>1){
 			genreString = genreString.substring(1);
 		}
-		adapter.add(new ClubListItem(R.drawable.item_bg, c.getName(),genreString));	
+		return genreString;
 	}
 }
--- PartySenseApp/src/party/sense/app/PartySenseDetailsActivity.java
+++ PartySenseApp/src/party/sense/app/PartySenseDetailsActivity.java
@@ -5,8 +5,13 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class PartySenseDetailsActivity extends Activity {
-
-	TextView tvClubName;
+	public static final String BUNDLE_ID_CLUB_NAME = "ClubName";
+	public static final String BUNDLE_ID_CLUB_GENRE = "ClubGenre";
+	public static final String BUNDLE_ID_CLUB_ADDRESS = "ClubAddress";
+	public static final String BUNDLE_ID_CLUB_WEBSITE = "ClubWebsite";
+	public static final String BUNDLE_ID_CLUB_DESCRIPTION = "ClubDescription";
+	
+	TextView tvClubName, tvClubAddress, tvClubGenres, tvClubDescription;
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -14,10 +19,16 @@ public class PartySenseDetailsActivity extends Activity {
 	    setContentView(R.layout.layout_club_details);
 	    
 	    tvClubName = (TextView) findViewById(R.id.txtClubName);
+	    tvClubGenres = (TextView) findViewById(R.id.txtGenre);
+	    tvClubAddress = (TextView) findViewById(R.id.txtStreetAddress);
+	    tvClubDescription = (TextView) findViewById(R.id.txtDescription);
 	    
 	    String clubName = getIntent().getExtras().getString("ClubName");
-	    tvClubName.setText(clubName);
 	    
+	    tvClubName.setText(getIntent().getExtras().getString(PartySenseDetailsActivity.BUNDLE_ID_CLUB_NAME));
+	    tvClubGenres.setText(getIntent().getExtras().getString(PartySenseDetailsActivity.BUNDLE_ID_CLUB_GENRE));
+	    tvClubAddress.setText(getIntent().getExtras().getString(PartySenseDetailsActivity.BUNDLE_ID_CLUB_ADDRESS));
+	    tvClubDescription.setText(getIntent().getExtras().getString(PartySenseDetailsActivity.BUNDLE_ID_CLUB_DESCRIPTION));
 	    // TODO Auto-generated method stub
 	}
 
