f258b68e911f988a35390d0e0d06732441dbbce0
Robert King <kingrobertking@gmail.com>
2013-01-20 17:23:07 +1300

    deleted lots of deprecated code. added TMP: C:\Users\ROBERT\AppData\Local\Temp to app.yaml (which will break it for everyone else).

--- PartySenseWebBackend/main.py
+++ PartySenseWebBackend/main.py
@@ -9,7 +9,7 @@ import cgi
 import quopri
 from datamodel import Club
 import json
-from lib import get_lines, get_form, get_json
+from lib import get_lines
 import csv
 from datetime import datetime
 
@@ -26,41 +26,13 @@ class ErrorHandler(BaseHandler):
         template = {'message': message}
         self.render_template("error.html", **template)
 
-class HomeHandler(BaseHandler):
-    def get(self):
-        template = {}
-        #self.render_template('home.html', **template)
-        self.render_template('index.html', **template)
-
-class AboutHandler(BaseHandler):
-    def get(self):
-        template = {}
-        #self.render_template('home.html', **template)
-        self.render_template('about.html', **template)
-
-class AdminHandler(BaseHandler):
-    def get(self):
-        template = {}
-        self.render_template('admin.html', **template)
-
 class BrowseDataModel(BaseHandler):
     def get(self):
         kinds = ["Club", "User", "Tag", "TrendingClubs"]
         models = []
         for kind in kinds:
             models.append("<hr/><h3>" + kind + "</h3>" + "<br/>".join(get_lines(kind)))
-
         self.response.write('<br/>'.join(models))
-
-
-class EditClubHandler(BaseHandler):
-    def get(self):
-        pass
-
-
-class JsonHandler(BaseHandler):
-    def get(self, kind):
-        self.response.write(get_json(kind))
 
 class ImportClubsHandler(BaseHandler):
     def get(self):
@@ -92,10 +64,7 @@ class ImportClubsHandler(BaseHandler):
         def same(a, b):
             return a is b or a == b
 
-
         for d, club in zip(dr, clubs):
-
-
             tags = d['Tags'].split(",")
             tags = [t.strip() for t in tags]
             if not club:
@@ -122,8 +91,6 @@ class ImportClubsHandler(BaseHandler):
             except:
                 return self.redirect('/error/long-lat-{0}'.format(club.name))
 
-
-
             for i, j in zip(olds, [getattr(club, prop, None) for prop in props]):
                 if i!=j:
                     try:
@@ -137,22 +104,6 @@ class ImportClubsHandler(BaseHandler):
             return self.redirect('/error/data-the-same-as-last-time-no-new-data')
         ndb.put_multi(clubs_to_save)
         self.response.write("success, updated {0} clubs".format(len(clubs_to_save)))
-
-class ClubsJsonHandler(BaseHandler):
-    def get(self):
-        clubs = Club.query().fetch()
-        json_dicts = []
-        props = ['name', 'website', 'tags', 'city', 'description', 'address', 'phone_number', 'longitude', 'latitude']
-        for club in clubs:
-            d = {}
-            for p in props:
-                if hasattr(club, p):
-                    val = getattr(club, p)
-                    if val:
-                        d[p] = str(val) if p in ('longitude', 'latitude') else val
-            assert d
-            json_dicts.append(d)
-        self.response.write(json.dumps(json_dicts, sort_keys=True,indent=4))
 
 class ClubsDeltaJsonHandler(BaseHandler):
     def get(self, year, month, day):
@@ -176,22 +127,27 @@ class ClubsDeltaJsonHandler(BaseHandler):
         self.response.write(json.dumps(json_dicts, sort_keys=True,indent=4))
 
 _routes = [
-    RedirectRoute('/', HomeHandler, name="home", strict_slash=True),
-    RedirectRoute('/about', AboutHandler, name="about", strict_slash=True),
     RedirectRoute('/error/<message>/', ErrorHandler, name="error", strict_slash=True),
-    RedirectRoute('/admin', AdminHandler, name="admin", strict_slash=True),
-    RedirectRoute('/api/all/<kind>', JsonHandler, name="json", strict_slash=True),
     RedirectRoute('/admin/browse-data-model', BrowseDataModel, name="browse-data-model", strict_slash=True),
-    #RedirectRoute('/admin/edit-kind/<kind>', EditKindHandler, name="edit-kind", strict_slash=True)
     RedirectRoute('/admin/import-clubs', ImportClubsHandler, name='import-clubs', strict_slash=True),
-    RedirectRoute('/api/clubs-dump', ClubsJsonHandler, name='clubs-dump', strict_slash=True),
     RedirectRoute('/api/clubs-delta/year/<year>/month/<month>/day/<day>', ClubsDeltaJsonHandler, name='clubs-dump', strict_slash=True)
 ]
+
+
+def html_handler(name):
+    file_name = name + ".html"
+    class HtmlHandler(BaseHandler):
+        def get(self):
+            template = {}
+            self.render_template(file_name, **template)
+    return HtmlHandler
+
+def dumb_routes(*args):
+    for name in args:
+        template = "/" + name
+        if template == "/index":
+            template = "/"
+        yield RedirectRoute(template, html_handler(name), name=name, strict_slash=True)
+
+_routes.extend(dumb_routes("index", "about", "admin"))
 app = webapp2.WSGIApplication(_routes, debug=True)
-
-
-#class EditKindHandler(BaseHandler):
-#    def get(self, kind):
-#        #gives basic form to cut and paste.
-#        template = {'form': get_form(kind)}
-#        self.render_template('edit-kind.html', **template)
