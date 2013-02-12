613659ffedb64bbc9801092ccb9a0e5739b8e65d
Ahmed Hilali <monstrobishi@gmail.com>
2013-02-12 23:41:00 +1300

    - Added PSCentral singleton and all-seeing-i library

---
+++ PartySenseAppiOS/PartySenseiOS/Classes/PSCentral.h
@@ -0,0 +1,13 @@
+//
+//  PSCentral.h
+//  PartySenseiOS
+//
+//  Created by Ahmed Hilali on 12/02/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PSCentral : NSObject
++(PSCentral*) sharedInstance;
+@end
