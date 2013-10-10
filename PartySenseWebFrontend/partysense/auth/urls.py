e4e1c8f41962a2722d6b6e03435e6c30f4e83edf
Brian Thorne <hardbyte@gmail.com>
2013-10-10 19:53:05 +1300

    Login/Logout fixes.
    Redirect to homepage on logount.
    Fix the login via email/password form.
    Fix a css style problem that was being applied to password incorrect error message.

--- PartySenseWebFrontend/partysense/auth/urls.py
+++ PartySenseWebFrontend/partysense/auth/urls.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.conf.urls import patterns, url
 
+from django.contrib.auth import views as auth_views
+
 from registration.backends.default.views import RegistrationView
 
 from partysense.auth.forms import CustomEmailRegistrationForm
@@ -11,6 +13,13 @@ urlpatterns = patterns('',
 
     # add a custom page that allows users to login with a password or oauth
     url(r'^login/$', password_login, name='password_login'),
+
+    url(
+        r'^logout/$',
+        auth_views.logout,
+        {'next_page': '/'},
+        name='password_logout',
+    ),
 
     # page that allows registration of new users with an email + password
     url(r'^register/$',
--- PartySenseWebFrontend/partysense/auth/views.py
+++ PartySenseWebFrontend/partysense/auth/views.py
@@ -5,8 +5,19 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
-from registration_email.forms import EmailAuthenticationForm
+from registration_email.forms import EmailAuthenticationForm as OriginalEmailAuthenticationForm
+
+
+class EmailAuthenticationForm(OriginalEmailAuthenticationForm):
+    def __init__(self, *args, **kwargs):
+
+        super(EmailAuthenticationForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper(self)
+        self.helper.add_input(Submit('submit', 'Login'))
 
 
 def password_login(request):
@@ -18,13 +29,16 @@ def password_login(request):
             logging.info("Email: {}".format(auth_form.data['username']))
             email = auth_form.data['username']
             password = auth_form.data['password']
-
             user = authenticate(username=email, password=password)
             login(request, user)
             return redirect("profile")
+        else:
+            logging.info("Invalid login via email/password")
+            logging.debug("Email was: {}".format(auth_form.data['username']))
     else:
         # create an unbound form
         auth_form = EmailAuthenticationForm()
+
     return render(request, 'auth/login.html',
                   {'formset': auth_form }
     )
