7947f9b29c5b3f0be18588c92edb2867b8a55a9d
Brian Thorne <hardbyte@gmail.com>
2013-10-11 18:39:41 +1300

    Move auth forms into auth/forms.py

--- PartySenseWebFrontend/partysense/auth/forms.py
+++ PartySenseWebFrontend/partysense/auth/forms.py
@@ -1,11 +1,18 @@
 from registration_email.forms import EmailRegistrationForm
-from django import forms
+from registration_email.forms import EmailAuthenticationForm as OriginalEmailAuthenticationForm
+
 from crispy_forms.helper import FormHelper, Layout
 from crispy_forms.layout import Div, Field, MultiField, Submit, HTML, Hidden
 
 CustomEmailRegistrationForm = EmailRegistrationForm
-
 CustomEmailRegistrationForm.helper = FormHelper()
 CustomEmailRegistrationForm.helper.add_input(Submit('submit', 'Register your account', css_class="btn-block"))
 
 
+class EmailAuthenticationForm(OriginalEmailAuthenticationForm):
+    def __init__(self, *args, **kwargs):
+
+        super(EmailAuthenticationForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper(self)
+        self.helper.add_input(Submit('submit', 'Login'))
