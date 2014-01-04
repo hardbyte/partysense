221c12ddc1b4bac8edeff1e0fdf01a01683082f1
Brian Thorne <hardbyte@gmail.com>
2014-01-04 16:52:08 +1300

    Add basic filtering of clubs to tastypie api.

--- PartySenseWebFrontend/partysense/club/api.py
+++ PartySenseWebFrontend/partysense/club/api.py
@@ -1,9 +1,12 @@
 # Tastypie API Resources
 from tastypie import fields
-from tastypie.resources import ModelResource
+from tastypie.resources import ModelResource, ALL
 from partysense.club.models import Club
 from partysense.api import UserResource
 from partysense.util.api import LocationResource
+
+# TODO http://django-tastypie.readthedocs.org/en/latest/geodjango.html
+
 
 class ClubResource(ModelResource):
 
@@ -13,3 +16,7 @@ class ClubResource(ModelResource):
     class Meta:
         queryset = Club.objects.all()
         resource_name = 'club'
+        filtering = {
+            'country': ALL,
+            'city': ALL
+        }
--- PartySenseWebFrontend/partysense/util/api.py
+++ PartySenseWebFrontend/partysense/util/api.py
@@ -2,6 +2,7 @@ from tastypie import fields
 from tastypie.resources import ModelResource
 from partysense.event.models import Location
 
+
 class LocationResource(ModelResource):
     class Meta:
         queryset = Location.objects.all()
