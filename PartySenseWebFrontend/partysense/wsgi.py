6b47bc4e22b0a9bbff1fb15bb1174425fd35f742
Brian Thorne <hardbyte@gmail.com>
2014-01-13 22:26:02 +1300

    Try avoid too much black magic with the sys.path

--- PartySenseWebFrontend/partysense/wsgi.py
+++ PartySenseWebFrontend/partysense/wsgi.py
@@ -1,19 +1,16 @@
 import os, sys, site
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "partysense.settings")
 
+# Add the site-packages of our deployment virtualenv
 site.addsitedir('/home/hardbyte/.virtualenvs/pslive/lib/python2.7/site-packages')
 
+# Add the app's directories to the python path
+sys.path.append('/home/hardbyte/webapps/partysense/partysense/PartySenseWebFrontend/')
+sys.path.append('/home/hardbyte/webapps/partysense/partysense/PartySenseWebFrontend/partysense')
+
+# Activate the virtual env
 activate_this = os.path.expanduser("~/.virtualenvs/pslive/bin/activate_this.py")
 execfile(activate_this, dict(__file__=activate_this))
-
-# Calculate the path based on the location of the WSGI script
-project = '/home/hardbyte/webapps/partysense/partysense/PartySenseWebFrontend/partysense'
-workspace = os.path.dirname(project)
-sys.path.append(workspace)
-
-sys.path = ['/home/hardbyte/webapps/partysense/partysense/PartySenseWebFrontend/',
-            '/home/hardbyte/webapps/partysense/partysense/PartySenseWebFrontend/partysense',
-            '/home/hardbyte/webapps/partysense'] + sys.path
 
 from django.core.handlers.wsgi import WSGIHandler
 application = WSGIHandler()
